e1bdf96b4fed682e1dc25bbcd065e526cf0b3bea
Jibin Thomas <jibin.thomas2706@gmail.com>
2019-03-24 14:13:13 +0530

    Styled footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,7 +3,18 @@ import React from "react"
 function Footer() {
   return (
     <footer>
-      <p>Designed and Developed by Jibin Thomas</p>
+      <hr style={{ maxWidth: "800px", margin: "16px auto" }} />
+      <div className="social">
+        <a href="https://github.com/jibin2706">Github</a>
+        <a href="https://twitter.com/real_jibin">Twitter</a>
+        <a href="https://www.linkedin.com/in/jibin-thomas-4b3496142/">
+          LinkedIn
+        </a>
+        <a href="https://www.instagram.com/jibin2706/">Instagram</a>
+      </div>
+      <div className="footer-info">
+        <p>© - Coded by hand and headaches</p>
+      </div>
     </footer>
   )
 }
