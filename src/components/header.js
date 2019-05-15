14b2ea69a81c710f0a3ddee1bb1e898e12dd79e4
Jibin Thomas <jibin.thomas2706@gmail.com>
2019-05-15 15:59:49 +0530

    Modularized link style

--- src/components/header.js
+++ src/components/header.js
@@ -3,8 +3,12 @@ import React from "react"
 const Header = () => (
   <header>
     <nav>
-      <a href="https://blog.jibin.tech">Blog</a>
-      <a href="#about">About</a>
+      <a className="link" href="https://blog.jibin.tech">
+        Blog
+      </a>
+      <a className="link" href="#about">
+        About
+      </a>
     </nav>
   </header>
 )
