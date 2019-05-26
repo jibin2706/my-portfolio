f98dfe40ce397b04c50ea09a9af7e9c77f658054
Jibin Thomas <jibin.thomas2706@gmail.com>
2019-05-26 20:44:48 +0530

    Configuration files changed

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,8 +22,8 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#f99f18`,
-        theme_color: `#f99f18`,
+        background_color: `#1f2933`,
+        theme_color: `#1f2933`,
         display: `minimal-ui`,
         icon: `src/images/favicon.png`, // This path is relative to the root of the site.
       },
