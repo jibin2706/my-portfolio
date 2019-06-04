ba33c3be6d067b52a84242ff392963132ef4f561
Jibin Thomas <jibin.thomas2706@gmail.com>
2019-06-04 08:33:29 +0530

    Fixed breaking on section heading on smaller screens

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -5,7 +5,10 @@ function Heading({ title }) {
   return (
     <div style={{ display: "flex", justifyContent: "center", margin: "16px" }}>
       <Line />
-      <h3 className="title-heading" style={{ display: "inline", margin: 0 }}>
+      <h3
+        className="title-heading"
+        style={{ display: "inline", margin: 0, whiteSpace: "nowrap" }}
+      >
         {title}
       </h3>
       <Line />
