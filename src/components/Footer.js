f4f628500db5b266c2399986f9ac710cddf27f5d
Jibin Thomas <jibin.thomas2706@gmail.com>
2019-06-05 06:05:24 +0530

    Removed social media svg and used text

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,27 +1,16 @@
 import React from "react"
-import instagram from "../images/instagram.svg"
-import github from "../images/github.svg"
-import twitter from "../images/twitter.svg"
-import linkedin from "../images/linkedin.svg"
 
 import "./footer.scss"
 
 function Footer() {
   return (
     <footer>
-      <hr style={{ maxWidth: "800px", margin: "16px auto" }} />
+      <hr style={{ maxWidth: "900px", margin: "16px auto" }} />
       <div className="social">
-        <a href="https://twitter.com/real_jibin">
-          <img src={twitter} alt="follow me on twitter" />
-        </a>
-        <a href="https://github.com/jibin2706">
-          <img src={github} alt="github" />
-        </a>
+        <a href="https://twitter.com/real_jibin">Twitter</a>
+        <a href="https://github.com/jibin2706">Github</a>
         <a href="https://www.linkedin.com/in/jibin-thomas-4b3496142/">
-          <img src={linkedin} alt="linkedin" />
-        </a>
-        <a href="https://www.instagram.com/jibin2706/">
-          <img src={instagram} alt="instagram" />
+          Linkedin
         </a>
       </div>
       <div className="footer-info">
