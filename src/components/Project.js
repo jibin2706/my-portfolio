d4042881678f52b22a27a368555ae429f82e0bca
Jibin Thomas <jibin.thomas2706@gmail.com>
2019-06-28 22:09:45 +0530

    Added webp image and refactored component

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,56 +1,68 @@
 import React from "react"
 
+import "./project.scss"
+
 import backslash from "../images/project/backslash.png"
+import backslashWebp from "../images/project/backslash.webp"
 import randomquote from "../images/project/randomquote.png"
+import randomquoteWebp from "../images/project/randomquote.webp"
 import phone from "../images/project/parkfinder.png"
+import phoneWebp from "../images/project/parkfinder.webp"
 
 function Project() {
   return (
-    <>
-      <div className="project-container">
-        <div className="card-container">
-          <div className="card">
-            <h2>Backslash Fest Website</h2>
-            <p>
-              Created a website for IT fest of Wilson College. Technology:
-              HTML5, CSS, jQuery
-            </p>
-            <a className="link" href="https://backslashfest.org">
-              Visit Project
-            </a>
-          </div>
-          <img src={backslash} alt="backslash website" />
+    <div>
+      <div className="card">
+        <div className="card__box">
+          <h2>Backslash Fest Website</h2>
+          <p className="card__text">
+            Created a website for IT fest of Wilson College. Technology: HTML5,
+            CSS, jQuery
+          </p>
+          <a className="link" href="https://backslashfest.org">
+            Visit Project
+          </a>
         </div>
-        <div className="card-container">
-          <div className="card">
-            <h2>Park Finder</h2>
-            <p>
-              IoT project to get find parking location on your android phone and
-              an easy to use api to access parking location's info.
-            </p>
-          </div>
+        <picture className="card__picture">
+          <source srcset={backslashWebp} type="image/webp" />
+          <source srcset={backslash} type="image/jpeg" />
+          <img src={backslash} alt="backslash website" />
+        </picture>
+      </div>
+      <div className="card">
+        <div className="card__box">
+          <h2>Park Finder</h2>
+          <p className="card__text">
+            IoT project to get find parking location on your android phone and
+            an easy to use api to access parking location's info.
+          </p>
+        </div>
+        <picture className="card__picture">
+          <source srcset={phoneWebp} type="image/webp" />
+          <source srcset={phone} type="image/jpeg" />
           <img
             src={phone}
             alt="park finder"
             style={{ zIndex: 1 }}
-            className="vertical-image"
+            className="card-picture--vertical"
           />
-        </div>
-        <div className="card-container">
-          <div className="card">
-            <h2>Random Quote Generator</h2>
-            <p>Generate random quotes made using React</p>
-            <a
-              className="link"
-              href="https://quote-gen.netlify.com/"
-            >
-              Visit Project
-            </a>
-          </div>
-          <img src={randomquote} alt="random quote generator" />
-        </div>
+        </picture>
       </div>
-    </>
+      <div className="card">
+        <div className="card__box">
+          <h2>Random Quote Generator</h2>
+          <p className="card__text">Generate random quotes made using React</p>
+          <a className="link" href="https://quote-gen.netlify.com/">
+            Visit Project
+          </a>
+        </div>
+        <picture className="card__picture">
+          <source srcset={randomquoteWebp} type="image/webp" />
+          <source srcset={randomquote} type="image/jpeg" />
+          <img src={randomquote} alt="random quote generator" />
+        </picture>
+      </div>
+    </div>
   )
 }
 
