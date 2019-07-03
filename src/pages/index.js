3aab3471ff7694877219c376753ca10cbf8488fc
Jibin Thomas <jibin.thomas2706@gmail.com>
2019-07-03 23:49:49 +0530

    Changed meta description and added about section

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import React from "react"
 import Layout from "../components/Layout"
 import SEO from "../components/SEO"
 import LandingScreen from "../components/LandingScreen"
+import About from "../components/About"
 import Skills from "../components/Skills"
 import Project from "../components/Project"
 import Blog from "../components/Blog"
@@ -12,8 +13,9 @@ import "../styles/style.scss"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Jibin Thomas | Front-End Developer and Casual Blogger" />
+    <SEO title="Jibin Thomas — Front-End Developer and Casual Blogger" description="Jibin Thomas is a Front-End Web developer from Mumbai. You can reach out to him jibin.thomas2706@gmail and he is available of freelance work."/>
     <LandingScreen />
+    <About />
     <Skills />
     <Project />
     <Blog />
